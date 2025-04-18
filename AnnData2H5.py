a2d2b9b42ce28236aff30897eca87f177fdcb291
Arthur Ko <arthur5ko@gmail.com>
2025-04-18 12:59:51 -0700

    Update the function to handle key errors

--- AnnData2H5.py
+++ AnnData2H5.py
@@ -3,6 +3,7 @@ import anndata as ad
 import scanpy as sc
 import numpy as np
 import h5py
+import sys
 from typing import Optional
 
 ### the "features" requires id,name, feature_type datasets at minimum
@@ -20,6 +21,7 @@ A 10X Genomics' HDF5 file contains a "matrix" group with the following data:
 -id
 -name
 -feature_type
+-genome
 
 This function writes an AnnData object (adata) into an HDF5 file following the 10X Genomics' format.
 adata.var will be written to the h5 file, whereas, adata.obs will not be saved. 
@@ -32,6 +34,7 @@ Args:
 	feature_id_col (optional,str): Column in adata.var to be used as "id" data in the h5 file. Defaults to "gene_ids" if not provided.
 	feature_type_col (optional,str): Column in adata.var to be used as "feature_type" data in the h5 file. The default is "feature_types".
 	name_col (optional,str): Column in adata.var that will be used as "name" data in the h5 file. None is the default value and adata.var.index will be used. Both Scanpy and Seurat use "name" as index by default when reading 10X h5. 
+	genome_col (optional,str): Column in adata.var that will be used as "gnome" adata. "genome" is the defaulty value.
 	compression (optional,str): Compression type for HDF5 datasets.
 	compression_opts (optional, int): Compression level (e.g., for gzip 0-9). Defaults to 4. 
 
@@ -39,18 +42,33 @@ Returns:
 	None. adata will be write to 'output' in 10X Genomics style HDF5 file.
 
 """
-def writeAdata_10Xh5(adata: ad.AnnData ,output:str, feature_id_col: Optional[str]="gene_ids", feature_type_col: Optional[str]="feature_types",name_col: Optional[str]=None,compression: str="gzip",compression_opts: int=4):
+def writeAdata_10Xh5(adata: ad.AnnData ,output:str, feature_id_col: Optional[str]="gene_ids", feature_type_col: Optional[str]="feature_types",name_col: Optional[str]=None,genome_col: Optional[str]="genome",compression: str="gzip",compression_opts: int=4):
 	
 	matrix=adata.X.T ## transpose adata.X to be in the same dimensions as 10X
 	barcodes=adata.obs.index.to_numpy().astype(bytes) ## must be bytes
 	
 	feature_tags={}
 	use_feature_tag_keys=[]
+	
+	try:
+		feature_tags["id"]=adata.var[feature_id_col].astype(str).to_numpy().astype(bytes)
+		use_feature_tag_keys.append("id")
+	except KeyError:
+		print("Error: the \"%s\" column does not exist in adata.var."%(feature_id_col))
+		return None
+	try:
+		feature_tags["feature_type"]=adata.var[feature_type_col].astype(str).to_numpy().astype(bytes)
+		use_feature_tag_keys.append("feature_type")
+	except KeyError:
+		print("Error: the \"%s\" column does not exist in adata.var."%(feature_type_col))
+		return None
+	try:
+		feature_tags["genome"]=adata.var[genome_col].astype(str).to_numpy().astype(bytes)
+		use_feature_tag_keys.append("genome")
+	except KeyError:
+		print("Error: the \"%s\" column does not exist in adata.var."%(genome_col))
+		return None
 
-	feature_tags["id"]=adata.var[feature_id_col].astype(str).to_numpy().astype(bytes)
-	use_feature_tag_keys.append("id")
-	feature_tags["feature_type"]=adata.var[feature_type_col].astype(str).to_numpy().astype(bytes)
-	use_feature_tag_keys.append("feature_type")
 	if name_col is None: ## use adat.var.index for the "name" dataset if not defined
 		feature_tags["name"]=adata.var.index.astype(str).to_numpy().astype(bytes)
 	else:
@@ -59,7 +77,7 @@ def writeAdata_10Xh5(adata: ad.AnnData ,output:str, feature_id_col: Optional[str
 	
 	### add the rest of adata.var to h5
 	for i in adata.var.columns:
-		if(i not in [feature_id_col, feature_type_col, name_col]): ## skip data already added
+		if(i not in [feature_id_col, feature_type_col, name_col,genome_col]): ## skip data already added
 			feature_tags[i]=adata.var[i].astype(str).to_numpy().astype(bytes)
 			use_feature_tag_keys.append(i)
 		else:
